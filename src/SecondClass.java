34b3f935db9b157c509a9492e6006e27e5fe7176
kwrona05 <kwrona05@gmail.com>
2025-04-15 19:12:28 +0200

    Practicing basics of conditions

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,21 +1,21 @@
 public class SecondClass {
     public static void main(String[] args) {
-        System.out.println("Hello, Kacper");
+        int clientAge = 20;
+        int balance = 200;
+        int price = 150;
 
-        boolean isAlien = false;
-        if (!isAlien) {
-            System.out.println("It is an alien!");
-            System.out.println("And I am scared of aliens");
+        if((clientAge >= 18) && (balance >= price)) {
+            System.out.println("You can buy beer");
         }
 
-        int topScore = 90;
-        if (topScore >= 90) {
-            System.out.println("You have high score!");
+        if((clientAge < 18) || (balance < price)) {
+            System.out.println("You can't buy beer");
         }
 
-        int secondTopScore = 80;
-        if (topScore > secondTopScore && topScore < 100) {
-            System.out.println("Greater then second top score but less then 100!");
+        boolean balancePlus = balance > 0 ? true : false;
+
+        if(balancePlus) {
+            System.out.println("That's nice");
         }
     }
 }
